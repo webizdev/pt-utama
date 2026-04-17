da6c5400f62ac1153709e3fa1ce6583d076e0ab2
webiz_dev <arifinamruna2@gmail.com>
2026-04-17 15:46:31 +0800

    Fix: Sitemap fetching issue resolve

--- sitemap.php
+++ sitemap.php
@@ -1,17 +1,26 @@
 <?php
-header("Content-type: text/xml");
-$baseUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+ob_start();
+header("Content-type: text/xml; charset=utf-8");
 
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+// Robust URL detection
+$protocol = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') ? "https" : "http";
+$host = $_SERVER['HTTP_HOST'];
+$baseUrl = $protocol . "://" . $host;
 
-// Home Page
-echo '<url>';
-echo '<loc>' . $baseUrl . '/</loc>';
-echo '<lastmod>' . date('Y-m-d') . '</lastmod>';
-echo '<changefreq>weekly</changefreq>';
-echo '<priority>1.0</priority>';
-echo '</url>';
+// If GSC still has issues, we can hardcode the domain if needed
+// $baseUrl = "https://ptutama.my.id";
 
-echo '</urlset>';
+echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc><?php echo $baseUrl; ?>/</loc>
+        <lastmod><?php echo date('Y-m-d'); ?></lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+    </url>
+</urlset>
+<?php
+ob_end_flush();
+exit;
 ?>
